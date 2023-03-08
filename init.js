980ea00a14f13e80ff1f2bd977620e5c8c0d72eb
Dakhm3 <123Rasool.souri@gmail.com>
2023-03-08 19:36:30 +0330

    run server

---
+++ init.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+globalThis.utils = require("./utils/utils");
+
+const logger = require("./utils/logger")
+
+
+const loggerInvokeResult = logger.init();
+globalThis.logger = logger;
+
+
+if (loggerInvokeResult) {
+    logger.log(loggerInvokeResult);
+}
+
+
+server.listen(process.env.PORT)
